b534b1fc99d1fe14ace3550ad6a51a05fd950f70
kazukimaeda26 <kazukimaeda26@gmail.com>
2020-11-03 16:28:50 +0900

    seedファイルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Theme.create(content: "土日にあったこと", week:"mon", span:"mor")
+Theme.create(content: "おすすめの学習方法、最近効果のあった学習法", week:"tue", span:"mor")
+Theme.create(content: "オリジナルアプリ（学習方法）の改善点", week:"wed", span:"mor")
+Theme.create(content: "おすすめの休憩方法", week:"thu", span:"mor")
+Theme.create(content: "最近失敗したこと、失敗した時間の使い方", week:"fri", span:"mor")
+
+Theme.create(content: "最近あったよかったこと", week:"mon", span:"eve")
+Theme.create(content: "好きな名言", week:"tue", span:"eve")
+Theme.create(content: "プログラミングを勉強してやりたいこと", week:"wed", span:"eve")
+Theme.create(content: "オリジナルアプリ（学習方法）のアピールポイント", week:"thu", span:"eve")
+Theme.create(content: "土日の目標・予定", week:"fri", span:"eve")
