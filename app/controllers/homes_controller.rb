ea20f948fcf493dc9427b5a025a0a414ed68611e
kazukimaeda26 <kazukimaeda26@gmail.com>
2020-11-03 16:29:54 +0900

    曜日によってテーマを取得

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,28 @@
 class HomesController < ApplicationController
-  before_action :set_theme, :index
   def index
-  end
-
-  private
-  def set_theme
-    now = Time.now
+    now = Time.now.wday
+    case now
+    when 0 then
+      @themeMor = "今日は日曜日です。朝礼はありません。"
+      @themeEve = "今日は日曜日です。夕礼はありません。"
+    when 1 then
+      @themeMor = Theme.find_by(week: "mon", span: "mor")
+      @themeEve = Theme.find_by(week: "mon", span: "eve")
+    when 2 then
+      @themeMor = Theme.find_by(week: "tue", span: "mor")
+      @themeEve = Theme.find_by(week: "tue", span: "eve")
+    when 3 then
+      @themeMor = Theme.find_by(week: "wed", span: "mor")
+      @themeEve = Theme.find_by(week: "wed", span: "eve")
+    when 4 then
+      @themeMor = Theme.find_by(week: "thu", span: "mor")
+      @themeEve = Theme.find_by(week: "thu", span: "eve")
+    when 5 then
+      @themeMor = Theme.find_by(week: "fri", span: "mor")
+      @themeEve = Theme.find_by(week: "fri", span: "eve")
+    when 6 then
+      @themeMor = "今日は土曜日です。朝礼はありません。"
+      @themeEve = "今日は土曜日です。夕礼はありません。"
+    end
   end
 end
